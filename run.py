20d17e6ad1dccc7a07ea8d3fe9a3e5be9098a17c
Rubén Zorrilla <rubenzorrillamartinez@hotmail.com>
2024-02-29 18:35:50 +0100

    Minors (not working yet)

--- run.py
+++ run.py
@@ -12,6 +12,10 @@ def CalculateDeltaTime(rho, mu, cell_size, velocities, target_cfl, target_fourie
         if norm_v > max_v:
             max_v = norm_v
 
+    tol = 1.0e-12
+    if max_v < tol:
+        max_v = tol
+
     cfl_dt_x = cell_size[0] * target_cfl / max_v
     cfl_dt_y = cell_size[1] * target_cfl / max_v
     cfl_dt_z = cell_size[2] * target_cfl / max_v if cell_size[2] > 1.0e-12 else 1.0e8
@@ -34,7 +38,7 @@ def GetButcherTableau():
     return A, B, C
 
 # Problem data
-end_time = 1.0e0
+end_time = 5.0e-1
 init_time = 0.0
 
 # Material data
@@ -43,7 +47,7 @@ rho = 1.293e0
 
 # Mesh data
 box_size = [1.0,1.0,None]
-box_divisions = [10,10,None]
+box_divisions = [3,2,None]
 cell_size = [i/j if i is not None else 0.0 for i, j in zip(box_size, box_divisions)]
 if box_size[2] == None:
     dim = 2
@@ -132,7 +136,16 @@ for i_cell in range(cells.shape[0]):
             divergence_operator[i_cell, node*dim + d] = cell_gradient_operator[i_node*dim + d]
         i_node += 1
 
+# Set initial conditions
+for i_node in range(num_nodes):
+    v[i_node, :] = [0.0,0.0,0.0]
+    v_n[i_node, :] = [1.0,0.0,0.0]
+    #y_coord = nodes[i_node][1]
+    #v[i_node, :] = [4.0*y_coord*(1.0-y_coord),0.0,0.0]
+    #v_n[i_node, :] = [4.0*y_coord*(1.0-y_coord),0.0,0.0]
+
 # Set velocity fixity vector (0: free ; 1: fixed) and BCs
+# Note that these overwrite the initial conditions above
 fixity = np.zeros((num_nodes*dim, 1), dtype=int)
 
 tol = 1.0e-6
@@ -145,11 +158,6 @@ for i_node in range(num_nodes):
         v_n[i_node, :] = [1.0,0.0,0.0]
     if ((node[1] < tol) or (node[1] > (1.0-tol))): # Top and bottom walls
         fixity[i_node*dim + 1] = 1 # y-velocity
-
-# Set initial conditions
-for i_node in range(num_nodes):
-    v[i_node, :] = [1.0,0.0,0.0]
-    v_n[i_node, :] = [1.0,0.0,0.0]
 
 # Set forcing term
 for i_node in range(num_nodes):
@@ -171,7 +179,9 @@ current_step = 1
 current_time = init_time
 rk_A, rk_B, rk_C = GetButcherTableau()
 while current_time < end_time:
-    dt = CalculateDeltaTime(rho, mu, cell_size, v_n, 0.9, 0.9)
+    # Compute time increment with CFL and Fourier conditions
+    # Note that we use the current step velocity to be updated as it equals the previous one at this point
+    dt = CalculateDeltaTime(rho, mu, cell_size, v, 0.2, 0.2)
     print(f"### Step {current_step} - time {current_time} - dt {dt} ###\n")
 
     # Calculate intermediate residuals
@@ -179,7 +189,8 @@ while current_time < end_time:
     rk_res = np.zeros((num_nodes*dim, rk_num_steps))
     for rk_step in range(rk_num_steps):
         # Calculate current step velocity for residual calculation
-        rk_step_time = current_time + rk_C[rk_step] * dt
+        rk_theta = rk_C[rk_step]
+        rk_step_time = current_time + rk_theta * dt
 
         rk_v = np.zeros((num_nodes*dim, 1))
         for i_step in range(rk_step):
@@ -196,7 +207,7 @@ while current_time < end_time:
                     rk_v[aux_i] *= dt / lumped_mass_vector[aux_i, 0]
                     rk_v[aux_i] += v_n[i,d]
                 else:
-                    rk_v[aux_i] = v_n[i,d]
+                    rk_v[aux_i] = rk_theta * v[i,d] + (1.0 - rk_theta) * v_n[i,d]
 
         # Calculate current step residual
         for i_cell in range(num_cells):
@@ -233,8 +244,6 @@ while current_time < end_time:
                     v[i, d] += rk_B[rk_step] * rk_res[aux_i, rk_step]
                 v[i, d] *= dt / lumped_mass_vector[aux_i, 0]
                 v[i, d] += v_n[i, d]
-            else:
-                v[i, d] = v_n[i, d]
 
     # Solve pressure update
     delta_p_rhs = np.zeros((num_cells, 1))
@@ -253,8 +262,6 @@ while current_time < end_time:
             if fixity[aux_i] == 0:
                 for j in range(num_cells):
                     v[i, d] += dt * gradient_operator[aux_i,j] * delta_p[j,0] / lumped_mass_vector[aux_i,0]
-            else:
-                v[i, d] = v_n[i, d]
 
     print(f"v: ", v)
     print(f"p: ", p)
@@ -280,7 +287,7 @@ while current_time < end_time:
     current_step += 1
     current_time += dt
 
-    if current_step > 0:
+    if current_step > 3:
         break
 
 # Finalize results
