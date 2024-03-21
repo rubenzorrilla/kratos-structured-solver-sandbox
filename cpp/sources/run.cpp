866538914b7ff7fba0520bcd0915cba4c0d378e3
rubenzorrilla <rubenzorrillamartinez@hotmail.com>
2024-03-21 10:21:51 +0100

    Advances

--- cpp/sources/run.cpp
+++ cpp/sources/run.cpp
@@ -109,5 +109,82 @@ int main()
         throw std::logic_error("3D case not implemented yet");
     }
 
+    Eigen::Array<bool, Eigen::Dynamic, 1> surrogate_cells(num_cells);
+    surrogate_cells.setZero();
+    if constexpr (dim == 2) {
+        std::array<int,4> cell_node_ids;
+        for (unsigned int i = 0; i < box_divisions[0]; ++i) {
+            for (unsigned int j = 0; j < box_divisions[1]; ++j) {
+                CellUtilities::GetCellNodesGlobalIds(i, j, box_divisions, cell_node_ids);
+                const auto& r_cell_node_dist = distance(cell_node_ids);
+                if (std::none_of(r_cell_node_dist.cbegin(), r_cell_node_dist.cend(), [](const double x){return x < 0.0;})) {
+                    for (int node_id : cell_node_ids) {
+                        if (surrogate_nodes(node_id)) {
+                            surrogate_cells(CellUtilities::GetCellGlobalId(i, j, box_divisions)) = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        throw std::logic_error("3D case not implemented yet");
+    }
+
+    // Calculate the distance vectors in the surrogate boundary nodes
+    Eigen::Vector<double, dim> aux_dir;
+    Eigen::Array<double, Eigen::Dynamic, dim> distance_vects(num_nodes, dim);
+    for (unsigned int i_node = 0; i_node < num_nodes; ++i_node) {
+        if (surrogate_nodes(i_node)) {
+            const auto& r_coords = nodal_coords.row(i_node);
+            aux_dir = cyl_orig - r_coords;
+            aux_dir /= aux_dir.norm();
+            distance_vects.row(i_node) = distance[i_node] * aux_dir;
+        }
+    }
+
+    // Apply fixity in the interior nodes
+    for (unsigned int i_node = 0; i_node < num_nodes; ++i_node) {
+        if (distance[i_node] < 0.0) {
+            fixity.row(i_node).setConstant(true);
+        }
+    }
+
+    // Apply fixity in the surrogate boundary nodes
+    for (unsigned int i_node = 0; i_node < num_nodes; ++i_node) {
+        if (surrogate_nodes(i_node)) {
+            fixity.row(i_node).setConstant(true);
+        }
+    }
+
+    // Deactivate the interior and intersected cells (SBM-like resolution)
+    if constexpr (dim == 2) {
+        std::array<int,4> cell_node_ids;
+        Eigen::Array<bool, Eigen::Dynamic, Eigen::Dynamic> active_cells(box_divisions[0], box_divisions[1]);
+        for (unsigned int i = 0; i < box_divisions[0]; ++i) {
+            for (unsigned int j = 0; j < box_divisions[1]; ++j) {
+                CellUtilities::GetCellNodesGlobalIds(i, j, box_divisions, cell_node_ids);
+                const auto cell_fixity = fixity(cell_node_ids, Eigen::all);
+                bool is_fixed = true;
+                for (unsigned int i_fix = 0; i_fix < cell_fixity.rows(); ++i_fix) {
+                    for (unsigned int j_fix = 0; j_fix < cell_fixity.cols(); ++j_fix) {
+                        if (!cell_fixity(i_fix, j_fix)) {
+                            is_fixed = false;
+                            break;
+                        }
+                    }
+                }
+                active_cells(i, j) = is_fixed;
+            }
+        }
+    } else {
+        throw std::logic_error("3D case not implemented yet");
+    }
+
+    // Set forcing term
+    f.setZero();
+
+
+
     return 0;
 }
