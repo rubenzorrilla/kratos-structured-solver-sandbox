b3bc60d64d98d0d1fba03fd26a7f1ec08e6f0436
Carlos Roig <roigcarlo@gmail.com>
2024-08-02 08:20:50 +0200

    Adding ninja and sycl flags

--- cpp/configure.sh
+++ cpp/configure.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# Load intel libraries
+source /home/roigcarlo/intel/oneapi/2024.0/oneapi-vars.sh intel64
+
 # Set compiler
-export CC=/usr/bin/clang-17
-export CXX=/usr/bin/clang++-17
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
 
 # Set variables
-export SOURCE_DIR="/home/rzorrilla/kratos-structured-solver-sandbox/cpp"
+export SOURCE_DIR="/home/roigcarlo/kratos-structured-solver-sandbox/cpp"
 export BUILD_DIR="${SOURCE_DIR}/build"
 #export FFTW
 
 # Set basic configuration
 export BUILD_TYPE="Release"
+
+# Set basic sycl flags to allow GCGPU
+export SYCL_FLAGS="-fsycl -fsycl-targets=nvptx64-nvidia-cuda -fsycl-unnamed-lambda"
 
 # Clean
 clear
@@ -19,10 +25,12 @@ rm -rf "${BUILD_DIR}/${BUILD_TYPE}/CMakeCache.txt"
 rm -rf "${BUILD_DIR}/${BUILD_TYPE}/CMakeFiles"
 
 # Configure
+# -G: Generator tool
 # -H: directory where the code is
 # -B: build directory (temporary files during compilation)
 # -DCMAKE_INSTALL_PREFIX: installation directory (where the result of the compilation will appear)
-cmake -H"${SOURCE_DIR}" -B"${BUILD_DIR}/${BUILD_TYPE}" -DCMAKE_INSTALL_PREFIX="${SOURCE_DIR}/bin"
+# -DCMAKE_CXX_FLAGS: flags for the C++ compiler
+cmake -G Ninja -H"${SOURCE_DIR}" -B"${BUILD_DIR}/${BUILD_TYPE}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_INSTALL_PREFIX="${SOURCE_DIR}/bin" -DCMAKE_CXX_FLAGS="${SYCL_FLAGS}"
 
 # Buid and install
 cmake --build "${BUILD_DIR}/${BUILD_TYPE}" --target install -- -j$(nproc)
